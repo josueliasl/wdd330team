c1a46bec4bd83f3b0e854b05498423d5a5976116
josueliasl <josue.elias.leyva@gmail.com>
2025-11-08 21:46:12 -0600

    Fix import statements in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import { ProductData } from "./ProductData.mjs";
+import ProductData from "./ProductData.mjs";
 const productDataInstance = new ProductData();
 
-import { ProducTList } from "./ProductList.mjs";
+import ProductList from "./ProductList.mjs";
 const productListInstance = new ProductList();
 
 import Alert from "./alert.mjs";
