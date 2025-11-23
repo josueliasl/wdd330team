5418418440d683d72e02f717a09336f35f343269
JOSUE ELIAS LEYVA GONZALEZ <167658323+josueliasl@users.noreply.github.com>
2025-11-22 19:51:45 -0600

    Enhance cart functionality with alerts for updates

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, alertMessage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
@@ -10,17 +10,30 @@ function addProductToCart(product) {
     cartItems = [];
   }
 
-  cartItems.push(product);
+
+  const existingItemIndex = cartItems.findIndex(item => item.Id === product.Id);
+  
+  if (existingItemIndex !== -1) {
+   
+    cartItems[existingItemIndex].quantity = (cartItems[existingItemIndex].quantity || 1) + 1;
+    alertMessage(`${product.Name} quantity updated! (Now ${cartItems[existingItemIndex].quantity})`, false);
+  } else {
+   
+    product.quantity = 1;
+    cartItems.push(product);
+    alertMessage(`${product.Name} added to cart!`, false);
+  }
+
   setLocalStorage("so-cart", cartItems);
 }
 
-// add to cart button event handler
+
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
